27995928852bd539bec1a3a718706f4bcd2f19e6
alexandruoliva <olivaalexandru@yahoo.com>
2021-05-09 00:34:08 +0300

    Created project

---
+++ app/src/main/java/com/oliva/newsreader/MainActivity.java
@@ -0,0 +1,21 @@
+package com.oliva.newsreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.oliva.newsreader.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
