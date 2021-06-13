e20092a16efbebd581e2f1936e30c09bbbb84d3e
alexandruoliva <olivaalexandru@yahoo.com>
2021-06-13 12:05:29 +0300

    Imported room and created Article entities

---
+++ data/src/main/java/com/oliva/data/model/entities/ArticleEntity.java
@@ -0,0 +1,22 @@
+package com.oliva.data.model.entities;
+
+import androidx.annotation.Nullable;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "articles")
+public class ArticleEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    @Nullable
+    public Integer id;
+
+    public String title;
+
+    public String content;
+
+    public String imageUrl;
+
+    public String description;
+
+}
