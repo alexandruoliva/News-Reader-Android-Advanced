931e33bbcb1dc3461d647089a5d6b1ab95e02613
alexandruoliva <olivaalexandru@yahoo.com>
2021-06-26 10:49:42 +0300

    Hided action bar in main activity

--- app/src/main/java/com/oliva/newsreader/MainActivity.java
+++ app/src/main/java/com/oliva/newsreader/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.main_activity);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
